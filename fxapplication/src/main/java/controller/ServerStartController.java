aced17ead06441b59586d3ca8003042631a6e76e
seternate <seternate@gmail.com>
2019-02-26 14:35:36 +0100

    enter can now be used on serverstartstage

--- fxapplication/src/main/java/controller/ServerStartController.java
+++ fxapplication/src/main/java/controller/ServerStartController.java
@@ -3,6 +3,8 @@ package controller;
 import entities.Game;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class ServerStartController {
     private Game game;
@@ -23,5 +25,11 @@ public class ServerStartController {
     @FXML
     private void startServer(){
         ApplicationManager.startServer(game, txtParameters.getText());
+    }
+
+    @FXML
+    private void enter(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER)
+            startServer();
     }
 }
