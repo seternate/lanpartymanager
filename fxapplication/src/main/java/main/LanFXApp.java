403a8daf1da19ddde999ce04a76d2d35b35a4ba0
seternate <seternate@gmail.com>
2019-02-28 08:38:02 +0100

    Added Javadocs

--- fxapplication/src/main/java/main/LanFXApp.java
+++ fxapplication/src/main/java/main/LanFXApp.java
@@ -4,6 +4,9 @@ import controller.ApplicationManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Application main-method entrance.
+ */
 public class LanFXApp extends Application {
 
     public static void main(String[] args) {
