bd50a81de77ad9cfc349736f9807b09a60472ace
seternate <seternate@gmail.com>
2019-02-28 08:26:18 +0100

    Added Javadocs

--- fxapplication/src/main/java/stages/LoginStage.java
+++ fxapplication/src/main/java/stages/LoginStage.java
@@ -10,8 +10,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * LoginStage class for the login window at the start and for the settings window accessed by the main stage.
+ */
 public class LoginStage extends Stage {
-
     public LoginStage(){
         super();
         FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("login.fxml"));
@@ -25,11 +27,14 @@ public class LoginStage extends Stage {
         if (icon != null) {
             getIcons().add(new Image(icon));
         }
+        /*
+            Changing stage title based on the opening.
+            If entered from startup the login window opened, entered from main stage the settings window opened.
+         */
         if(!ApplicationManager.isMainstage())
             setTitle("Lanpartymanager - Login");
         else
             setTitle("Settings");
         setResizable(false);
     }
-
 }
