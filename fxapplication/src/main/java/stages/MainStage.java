33f2f94972a8ea8e225d7c28edaa7e3854ead383
seternate <seternate@gmail.com>
2019-03-11 20:21:56 +0100

    added window size and position remembering

--- fxapplication/src/main/java/stages/MainStage.java
+++ fxapplication/src/main/java/stages/MainStage.java
@@ -7,10 +7,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.prefs.Preferences;
 
 /**
  * MainStage class for the main window of the application.
@@ -37,6 +40,24 @@ public class MainStage extends Stage {
         //MinHeight and MinWidth for the window
         setMinWidth(600);
         setMinHeight(400);
+
+        Preferences pref = Preferences.userRoot().node("MainStage");
+        double x = pref.getDouble("win_pos_x", (Screen.getPrimary().getVisualBounds().getWidth() - getWidth()) / 2);
+        double y = pref.getDouble("win_pos_y", (Screen.getPrimary().getVisualBounds().getHeight() - getHeight()) / 2);
+        double width = pref.getDouble("win_width", getMinWidth());
+        double height = pref.getDouble("win_height", getMinHeight());
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
+
+        this.setOnCloseRequest(event -> {
+            Preferences preferences = Preferences.userRoot().node("MainStage");
+            preferences.putDouble("win_pos_x", getX());
+            preferences.putDouble("win_pos_y", getY());
+            preferences.putDouble("win_width", getWidth());
+            preferences.putDouble("win_height", getHeight());
+        });
     }
     /**
      * Called if any new game received from the server and reloads the gametiles in the main window.
