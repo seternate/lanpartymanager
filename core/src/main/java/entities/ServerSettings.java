ad115393ad206be8f9c8007dff473f7e345473db
seternate <seternate@gmail.com>
2019-03-18 16:22:09 +0100

    added javadoc

--- core/src/main/java/entities/ServerSettings.java
+++ core/src/main/java/entities/ServerSettings.java
@@ -2,11 +2,16 @@ package entities;
 
 import java.io.IOException;
 
+/**
+ * Serves the server settings.
+ */
 public final class ServerSettings extends Settings {
-
+    /**
+     * Loads the server settings.
+     *
+     * @throws IOException if any error occurs while creating server settings.
+     */
     public ServerSettings() throws IOException {
         super(true);
-        if(getProperty("serverudp") == null && getProperty("servertcp") == null)
-            throw new IOException();
     }
 }
