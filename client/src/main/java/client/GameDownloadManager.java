bdbcb86df1cb4a7a3e2b555f9b85e64a1ef41573
seternate <seternate@gmail.com>
2019-03-21 07:54:30 +0100

    rewritten to extends from arraylist

---
+++ client/src/main/java/client/GameDownloadManager.java
@@ -0,0 +1,24 @@
+package client;
+
+import entities.game.Game;
+
+import java.util.ArrayList;
+
+//TODO
+final class GameDownloadManager extends ArrayList<GameDownload>{
+
+    @Override
+    public boolean add(GameDownload gameDownload){
+        gameDownload.setManager(this);
+        return super.add(gameDownload);
+    }
+
+    public GameDownload getDownload(Game game){
+        for(GameDownload download : this){
+            if(download.game.equals(game))
+                return download;
+        }
+        return null;
+    }
+
+}
