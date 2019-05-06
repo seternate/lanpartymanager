eba76f642a4318501fb17a9a3457a53df417b194
seternate <seternate@gmail.com>
2019-05-06 12:10:43 +0200

    javadoc update

--- core/src/main/java/entities/user/UserRunGamesList.java
+++ core/src/main/java/entities/user/UserRunGamesList.java
@@ -9,6 +9,13 @@ import entities.game.Game;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * {@code UserRunGamesList} handles all running {@link Game}.
+ *
+ * @author Levin Jeck
+ * @version 1.0
+ * @since 1.0
+ */
 @JsonDeserialize(keyUsing = UserKeyDeserializer.class)
 @JsonSerialize(keyUsing = UserKeySerializer.class)
 public class UserRunGamesList extends HashMap<User, List<Game>> {
