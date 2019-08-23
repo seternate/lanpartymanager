a3b42e84101792aed08094873917375d8cda4ade
seternate <seternate@gmail.com>
2019-08-23 16:54:09 +0200

    updated LoginStage

--- client/src/main/java/controller/LoginController.java
+++ client/src/main/java/controller/LoginController.java
@@ -2,7 +2,6 @@ package controller;
 
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
